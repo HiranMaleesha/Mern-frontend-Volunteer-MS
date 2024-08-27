010cd66b8fe1ad5478fe3de909e7ae290bc78dd6
HiranMaleesha <maleesha20010527@gmail.com>
2024-08-27 22:54:41 +0530

    sideBar deco

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Nav.css';
 import { Link } from 'react-router-dom';
+import { FaHome, FaUserPlus, FaUsers } from 'react-icons/fa'; // Import icons
 
 function Nav() {
   return (
@@ -8,16 +9,19 @@ function Nav() {
       <ul className='sidebar-ul'>
         <li className='sidebar-ll'>
           <Link to="/" className="active sidebar-a">
+            <FaHome className="sidebar-icon" />
             <h1>Home</h1>
           </Link>
         </li>
         <li className='sidebar-ll'>
           <Link to="/adduser" className="active sidebar-a">
+            <FaUserPlus className="sidebar-icon" />
             <h1>Add User</h1>
           </Link>
         </li>
         <li className='sidebar-ll'>
           <Link to="/userdetails" className="active sidebar-a">
+            <FaUsers className="sidebar-icon" />
             <h1>User Details</h1>
           </Link>
         </li>
